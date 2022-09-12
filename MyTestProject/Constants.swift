267057dd4c74126794d6b9c9bef33940313f5c66
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2022-09-12 04:07:30 +0300

    - set home view - set tableView and tableView cell - add constants file - add door model - add door Data

---
+++ MyTestProject/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  MyTestProject
+//
+//  Created by Oleksandr Smakhtin on 12.09.2022.
+//
+
+import Foundation
+
+struct K {
+    
+    struct DoorStatus {
+        static let unlocked = "Unlocked"
+        static let locked = "Locked"
+        static let unlocking = "Unlocking..."
+    }
+    
+    struct StatusImagesNames {
+        static let shieldLocked = "shieldLocked"
+        static let shieldUnlocked = "shieldUnlocked"
+        static let shieldUnlocking = "shieldUnlocking"
+        static let doorLocked = "doorLocked"
+        static let doorUnlocked = "doorUnlocked"
+        
+    }
+    
+}
---
+++ MyTestProject/MainVC.swift
@@ -0,0 +1,134 @@
+//
+//  MainVC.swift
+//  MyTestProject
+//
+//  Created by Oleksandr Smakhtin on 10.09.2022.
+//
+import SnapKit
+import UIKit
+
+class MainVC: UIViewController {
+
+    
+    let tableView = UITableView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        setView()
+        configureTableView()
+    }
+    
+    let stackView = UIStackView()
+    
+    func setView() {
+        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        //let stackView = UIStackView()
+        view.addSubview(stackView)
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(77.3)
+            make.right.equalToSuperview().inset(27.0)
+            make.left.equalToSuperview().inset(24.0)
+        }
+        
+        
+        
+        // Logo and setting stack
+        let logoAndSettingStack = UIStackView()
+        view.addSubview(logoAndSettingStack)
+        logoAndSettingStack.axis = .horizontal
+        logoAndSettingStack.distribution = .equalSpacing
+        stackView.addArrangedSubview(logoAndSettingStack)
+        
+        
+        
+        // Logo
+        let logoImageView = UIImageView()
+        logoImageView.image = UIImage(named: "interQR")
+        logoImageView.contentMode = .scaleAspectFit
+        logoAndSettingStack.addArrangedSubview(logoImageView)
+
+        
+        // Setting Btn
+        let settingBtn = UIButton()
+        settingBtn.setImage(UIImage(named: "Setting"), for: .normal)
+        logoAndSettingStack.addArrangedSubview(settingBtn)
+
+        
+        
+        
+        // Welcome and Home StackView
+        let welcomeAndHomeStack = UIStackView()
+        view.addSubview(welcomeAndHomeStack)
+        welcomeAndHomeStack.axis = .horizontal
+        welcomeAndHomeStack.distribution = .equalSpacing
+        welcomeAndHomeStack.spacing = 50
+        stackView.addArrangedSubview(welcomeAndHomeStack)
+        
+        
+        // Welcome Lbl
+        let welcomeLbl = UILabel()
+        welcomeLbl.text = "Welcome"
+        welcomeLbl.font = UIFont.systemFont(ofSize: 34, weight: .medium)
+        welcomeAndHomeStack.addArrangedSubview(welcomeLbl)
+        
+        // Home image
+        let homeImageView = UIImageView()
+        homeImageView.image = UIImage(named: "Home")
+        homeImageView.contentMode = .scaleAspectFill
+        welcomeAndHomeStack.addArrangedSubview(homeImageView)
+        
+        
+    }
+    
+    
+    
+    func configureTableView() {
+        view.addSubview(tableView)
+        // set delegates
+        setTableViewDelegates()
+        // register cell
+        tableView.register(DoorCell.self, forCellReuseIdentifier: "DoorCell")
+        // settings
+           // disable separator
+        tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+           // disable scroll indicator
+        tableView.showsVerticalScrollIndicator = false
+        tableView.rowHeight = 117 + 14
+        // set constrains
+        tableView.snp.makeConstraints { make in
+            make.right.equalToSuperview().inset(15)
+            make.left.equalToSuperview().inset(15)
+            make.bottom.equalToSuperview()
+            make.top.equalTo(stackView.snp_bottomMargin).offset(84)
+        }
+    }
+    
+    func setTableViewDelegates() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+
+
+}
+
+extension MainVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DoorCell") as! DoorCell
+        
+        return cell
+    }
+    
+    
+}
